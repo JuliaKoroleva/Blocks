cea4af6ce7ffb971c8c8cd7cacd8f91a6e68ca32
SofiaKelessidi <sofia_27@mail.ru>
2016-03-06 12:43:23 +0300

    NewGrids

--- Block/MainWindow.xaml.cs
+++ Block/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Block
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+             
+        }
     }
 }
