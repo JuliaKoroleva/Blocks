9a360a2942d7de52049393dd28f30ea1eaad7f38
JuliaKoroleva <juliakoroleva@inbox.ru>
2016-03-20 15:02:25 +0300

    удаление

--- Block/MainWindow.xaml.cs
+++ Block/MainWindow.xaml.cs
@@ -287,6 +287,7 @@ namespace Block
                     rect.Stroke = new SolidColorBrush(Colors.Gray);
                     rect.Width = cellWidth;
                     rect.Height = cellHeight;
+                    rect.Fill = new SolidColorBrush(Colors.White);
 
                     for (int j2 = 0; j2 < N; j2++)
                     {
@@ -304,6 +305,7 @@ namespace Block
                     rect.Stroke = new SolidColorBrush(Colors.Gray);
                     rect.Width = cellWidth;
                     rect.Height = cellHeight;
+                    rect.Fill = new SolidColorBrush(Colors.White);
 
                     for (int j2 = 0; j2 < N; j2++)
                     {
@@ -376,17 +378,17 @@ namespace Block
                 {
                     if (figure[i, j] != 0)
                     {
-                        if ((Ycell + i>9)&&(Xcell + j > 9))
+                        if ((Ycell + i > 9) && (Xcell + j > 9))
                             bigFigure[9, 9] = figure[i, j];
-                        else if (Ycell + i>9)
+                        else if (Ycell + i > 9)
                             bigFigure[9, Xcell + j] = figure[i, j];
                         else if (Xcell + j > 9)
                             bigFigure[Ycell + i, 9] = figure[i, j];
-                        else if ((Ycell + i<0)&&(Xcell + j <0))
-                            bigFigure[0,0] = figure[i, j];
-                        else if (Ycell + i<0)
+                        else if ((Ycell + i < 0) && (Xcell + j < 0))
+                            bigFigure[0, 0] = figure[i, j];
+                        else if (Ycell + i < 0)
                             bigFigure[0, Xcell + j] = figure[i, j];
-                        else if (Xcell + j <0)
+                        else if (Xcell + j < 0)
                             bigFigure[Ycell + i, 0] = figure[i, j];
                         else
                         bigFigure[Ycell + i, Xcell + j] = figure[i, j];                      
@@ -442,6 +444,7 @@ namespace Block
                 {
                     Redraw(bigFigure, canvasMain);
                     mainField = Add_Figure_To_Array(bigFigure, mainField);
+                    Delete_Rows_Columns(ref mainField, canvasMain);
 
                     canvasUpper1.Children.Clear();
                     currentFigureNumber1 = DrawFigures(r, canvasUpper1);
